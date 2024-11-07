ddf6b13a072996eee9b7d3709c9214e9ceb80301
Ratovonirina <eric.ratovonirina@gmail.com>
2024-11-07 13:51:45 +0330

    main : create entity for the two models User&Entity

---
+++ src/entityObject/entity/entityObject.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+import { User } from '../../user/entity/user.entity';
+
+@Entity()
+export class EntityObject {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column({ length: 20 })
+  siret: string;
+
+  @Column({ length: 250 })
+  keyLicence: string;
+
+  @Column({ length: 100 })
+  website: string;
+
+  @Column({ type: 'datetime' })
+  createdAt: Date;
+
+  @ManyToMany(() => User, (user) => user.entities)
+  @JoinTable() 
+  users: User[];
+}
---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany } from 'typeorm';
+import { EntityObject } from '../../entityObject/entity/entityObject.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 80 })
+  name: string;
+
+  @Column({ length: 50 })
+  firstName: string;
+
+  @Column({ length: 2 })
+  language: string;
+
+  @Column({ length: 100 })
+  email: string;
+
+  @Column({ length: 250 })
+  password: string;
+
+  @Column({ type: 'datetime' })
+  lastLogin: Date;
+
+  @Column({ type: 'datetime' })
+  createdAt: Date;
+
+  @ManyToMany(() => EntityObject, (entity) => entity.users)
+  entities: EntityObject[];
+}
